1d3335f3b8974f849ee08c808106ede8ec69c68f
Denis <dem_maverick177@mail.ru>
2021-10-15 12:38:38 +0300

    add all files

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+    $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-prev btn slick-btn"> <img src="images/arrow-left.svg" alt="arrow-left"></button>',
+		nextArrow: '<button type="button" class="slick-next btn slick-btn"> <img src="images/arrow-right.svg" alt="arrow-right"></button>'
+	});
+
+	$('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+	$('.menu-btn').on('click', function() {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+
+});
